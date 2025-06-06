53be6c0cf96f4da51fc8303e65fb8913384d2e2f
kushal <kushalpatel043@gmail.com>
2025-06-06 14:55:28 +0530

    EDA and training the model

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_details(error,error_details:sys):
     _,_,exc_tb =error_details.exc_info()
@@ -8,11 +8,10 @@ def error_message_details(error,error_details:sys):
         file_name,exc_tb.tb_lineno,str(error))
 
     return error_message
-    
+     
 class CutsomException(Exception):
     def __init__(self,error_message,error_details:sys):
         super().__init__(error_message)
         self.error_message=error_message_details(error_message,error_details=error_details)
-
     def __str__(self):
         return self.error_message
